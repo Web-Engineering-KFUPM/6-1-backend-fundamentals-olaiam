6ead359bf191cbcaa6245708a08558a0ac75bda3
olaiam <s202283060@kfupm.edu.sa>
2026-03-30 11:29:15 +0300

    TODO7

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -26,7 +26,12 @@ app.get('/about', (req, res) => {
 
 // TODO 7: Create the "/student" route
 
-
+app.get('/student', (req, res) => {
+  res.json({
+    name: 'Aisha',
+    major: 'Computer Science'
+  });
+});
 
 // TODO 4: Start the server on port 3000
 app.listen(3000, () => {
